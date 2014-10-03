fbdbd389354748863d51810799a2fefd2b3d194b
Chris Martin <ch.martin@gmail.com>
2014-10-03 05:35:06 -0400

    convert ConfigIncluderClasspath
    Obviously the "classpath" bit won't be directly relevant, but I think it
    might make sense to keep the classpath things around and repurpose them
    for loading config files via pkg_resources.

---
+++ hocon/ConfigIncluderClasspath.py
@@ -0,0 +1,23 @@
+class ConfigIncluderClasspath(object):
+    """
+     * Implement this <em>in addition to</em> {@link ConfigIncluder} if you want to
+     * support inclusion of files with the {@code include classpath("resource")}
+     * syntax. If you do not implement this but do implement {@link ConfigIncluder},
+     * attempts to load classpath resources will use the default includer.
+    """
+
+    def include_resources(self, context, what):
+        """
+        ConfigObject includeResources(ConfigIncludeContext context, String what);
+
+         * Parses another item to be included. The returned object typically would
+         * not have substitutions resolved. You can throw a ConfigException here to
+         * abort parsing, or return an empty object, but may not return null.
+         *
+         * @param context
+         *            some info about the include context
+         * @param what
+         *            the include statement's argument
+         * @return a non-null ConfigObject
+        """
+        raise NotImplementedError
