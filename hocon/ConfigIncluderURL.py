cbcc6d0b02b25f5c7713588107a993077a959661
Chris Martin <ch.martin@gmail.com>
2014-10-03 05:39:37 -0400

    convert ConfigIncluderURL

---
+++ hocon/ConfigIncluderURL.py
@@ -0,0 +1,23 @@
+class ConfigIncluderURL(object):
+    """
+     * Implement this <em>in addition to</em> {@link ConfigIncluder} if you want to
+     * support inclusion of files with the {@code include url("http://example.com")}
+     * syntax. If you do not implement this but do implement {@link ConfigIncluder},
+     * attempts to load URLs will use the default includer.
+    """
+
+    def include_url(self, context, what):
+        """
+        ConfigObject includeURL(ConfigIncludeContext context, URL what);
+
+         * Parses another item to be included. The returned object typically would
+         * not have substitutions resolved. You can throw a ConfigException here to
+         * abort parsing, or return an empty object, but may not return null.
+         *
+         * @param context
+         *            some info about the include context
+         * @param what
+         *            the include statement's argument
+         * @return a non-null ConfigObject
+        """
+        raise NotImplementedError
